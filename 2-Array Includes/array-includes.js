53662967f72df4ed5aa8f7f0fe70a9060b12adfe
Akash <akash.gutha@outlook.com>
2017-08-30 00:19:30 +0530

    added code

--- 2-Array Includes/array-includes.js
+++ 2-Array Includes/array-includes.js
@@ -1,1 +1,15 @@
 
+// Lesson : Array includes method in ES2016 (ES7)
+// Author : Akash Gutha
+
+const x = [NaN];
+
+console.log(x.includes(NaN));
+
+// older method
+console.log( x.indexOf(NaN) >= 0 )
+
+console.log([-0].includes(+0))
+
+const y = Uint8Array.of(1,5,6,6,7,8)
+console.log( y.includes(8) )
