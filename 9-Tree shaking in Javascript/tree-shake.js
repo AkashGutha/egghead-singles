8b2d9b0d0ae1c1a036e2ee283c9b601b704336e7
Akash Gutha <akash.gutha@outlook.com>
2017-09-08 02:23:15 +0530

    updated code to reflect the lesson

--- 9-Tree shaking in Javascript/tree-shake.js
+++ 9-Tree shaking in Javascript/tree-shake.js
@@ -2,48 +2,53 @@ var esprima = require("esprima");
 var estraverse = require("estraverse");
 var escodegen = require("escodegen");
 
-//  read our code into an object
+// read the index file
 var fs = require("fs");
 var code = fs.readFileSync("./index.js", {
   encoding: "UTF-8"
 });
 
-var fn_nodes = [];
-var call_nodes = [];
 
 // collect our functions and call expressions.
-var ast = esprima.parseScript(code, {}, function(node, meta) {
-  if (node.type === esprima.Syntax.FunctionDeclaration) {
-    fn_nodes.push(node);
-  }
-  if (node.type === esprima.Syntax.CallExpression) {
-    call_nodes.push(node);
-  }
-});
+var fn_nodes = [];
+var call_nodes = [];
+var ast = esprima.parseScript(code, {}, function(node){
+    if(node.type == esprima.Syntax.FunctionDeclaration){
+        // add it to the fns array
+        fn_nodes.push(node)
+    }
+    if(node.type == esprima.Syntax.CallExpression){
+        // add it to the fns array
+        call_nodes.push(node)
+    }
+})
 
 // get all our used functions
-var used_fns = [];
+var used_fns = []
 
 fn_nodes.forEach(function(fn_node) {
-  call_nodes.forEach(function(call_node) {
-    if (fn_node.id.name === call_node.callee.name && used_fns.indexOf(fn_node) < 0) {
-        used_fns.push(fn_node);
-      }
-    
-  });
+    call_nodes.forEach(function(call_node) {
+        
+        if(fn_node.id.name === call_node.callee.name && used_fns.indexOf(fn_node) <= 0){
+            // valid fn 
+            used_fns.push(fn_node);
+        }
+
+    }, this);
 }, this);
 
-// use estraverse to traverse adn delete the unused functions.
-var modified_ast = estraverse.replace(ast, {
-  enter: function(node) {
-    if (node.type === esprima.Syntax.FunctionDeclaration && used_fns.indexOf(node) === -1) {
-        this.remove();
-    }
-    return node;
-  }
-});
 
-// generate modified code.
+// use estraverse to traverse and delete the unused functions.
+var modified_ast = estraverse.replace(ast, {
+    enter: function(node){
+        if(node.type === esprima.Syntax.FunctionDeclaration && used_fns.indexOf(node) === -1){
+            this.remove();
+        }
+        return node;
+    }
+})
+
+// generate modified code. output
 var modified_code = escodegen.generate(modified_ast);
 
 
